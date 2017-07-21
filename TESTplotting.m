3b91666d5dd4f8073f7d0bc9642252e69acdf0ae
tim <tjhickey@brandeis.edu>
2017-07-21 14:27:44 -0400

    switch to Clean data for TESTplotting

--- TESTplotting.m
+++ TESTplotting.m
@@ -1,12 +1,13 @@
 subjects = [55,56,57,58,59,61,67,70,71,72];
 for i=[1:length(subjects)]
     figure(i)
-    dataset = createDataset(subjects(i),'M-S-R-O');
+    dataset = createCleanDataset(subjects(i),'M-S-R-O');
     for j=[1:4]
         subplot(4,1,j);
         musePlot(dataset{j}(:,2:21));
         hold on;
         plot(dataset{j}(:,1),'k.');
+        axis([0,12000,0,4]);
         hold off;
         title(strvcat(['subject = ',num2str(i),' and session = ',num2str(j)]));
     end
